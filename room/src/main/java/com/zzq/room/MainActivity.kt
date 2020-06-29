34474a9130d6b1eb07e90746259622b186c1052e
fmzq <787031941@qq.com>
2020-06-29 19:52:44 +0800

    test Room

---
+++ room/src/main/java/com/zzq/room/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.zzq.room
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.zzq.room.data.AppDatabase
+import com.zzq.room.data.User
+import com.zzq.room.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var index = 0L
+    private lateinit var userListAdapter: UserListAdapter
+    private lateinit var rvMain: RecyclerView
+    private var resetIndex = false
+    private val dataSource = ArrayList<User>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val userDao = AppDatabase.getInstance(this).userDao()
+        val allUser = userDao.getAllUser()
+        rvMain = binding.rvMain
+        rvMain.layoutManager = LinearLayoutManager(this)
+        userListAdapter = UserListAdapter()
+        rvMain.adapter = userListAdapter
+
+        subscribeUi()
+
+        binding.fabAdd.setOnClickListener {
+            resetIndex = true
+            index++
+            val user = User(index, "user_fab $index", System.currentTimeMillis(), "$index user_fab $index")
+            if ((user.userId % 2).toInt() == 0) {
+                user.address = user.name
+            }
+//            val currentList = userListAdapter.currentList
+//            val size = currentList.size
+//            currentList.add(size, user)
+//            userListAdapter.notifyItemInserted(size)
+            GlobalScope.launch {
+                userDao.addUser(user)
+            }
+        }
+
+        binding.fabAdd.setOnLongClickListener {
+            if (dataSource.isNotEmpty()) {
+                if (index >= 1) {
+                    index--
+                }
+                GlobalScope.launch { userDao.deleteUser(dataSource.last()) }
+                return@setOnLongClickListener true
+            }
+            return@setOnLongClickListener false
+        }
+
+        GlobalScope.launch {
+            val list = userDao.getAllUser2()
+            Log.e("tetetetete", "onCreate size: ${list.size}")
+        }
+    }
+
+    private fun subscribeUi() {
+        AppDatabase.getInstance(this).userDao().getAllUser().observe(this, Observer {
+            Log.e("tetetetete", "data change ${it.size}")
+            userListAdapter.submitList(it)
+            if (!resetIndex && it.isNotEmpty()) {
+                index = it.last().userId
+            }
+
+            dataSource.clear()
+            dataSource.addAll(it)
+            rvMain.smoothScrollToPosition(it.size - 1)
+        })
+    }
+
+}
---
+++ room/src/main/java/com/zzq/room/app/RoomApp.kt
@@ -0,0 +1,10 @@
+package com.zzq.room.app
+
+import android.app.Application
+
+class RoomApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
