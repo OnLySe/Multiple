f64de830ab41cffa94cd1ccd899d5930cc770768
fmzq <787031941@qq.com>
2020-09-04 17:48:37 +0800

    补充sourceSets配置

---
+++ gradlebuild/src/main/api21/com/zzq/gradlebuild/Test21.java
@@ -0,0 +1,11 @@
+package com.zzq.gradlebuild;
+
+import android.annotation.TargetApi;
+
+@TargetApi(21)
+public class Test21 {
+
+    public static int getTargetApi() {
+        return 21;
+    }
+}
--- util/src/main/java/com/zzq/util/ToastUtil.kt
+++ util/src/main/java/com/zzq/util/ToastUtil.kt
@@ -1,14 +1,39 @@
 package com.zzq.util
 
+import android.app.Application
 import android.content.Context
+import android.os.Handler
 import android.widget.Toast
 
 object ToastUtil {
-    fun shortToast(context: Context, message: String) {
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    private var sToast: Toast? = null
+    private val sHandler = Handler()
+
+    /**
+     * 可以在Application初始化就赋值
+     */
+    lateinit var app: Application
+    fun showToast(msg: String?) {
+        if (null == sToast) {
+            sToast = Toast.makeText(app, msg, Toast.LENGTH_SHORT)
+        } else {
+            sToast!!.setText(msg)
+        }
+        sToast!!.cancel()
+        sHandler.postDelayed({ sToast!!.show() }, 200)
     }
 
-    fun longToast(context: Context, message: String) {
-        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    fun showLongToast(msg: String?) {
+        if (null == sToast) {
+            sToast = Toast.makeText(app, msg, Toast.LENGTH_LONG)
+        } else {
+            sToast!!.setText(msg)
+        }
+        sToast!!.cancel()
+        sHandler.postDelayed({ sToast!!.show() }, 200)
+    }
+
+    fun showToast(context: Context?, msg: String?) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
     }
 }
