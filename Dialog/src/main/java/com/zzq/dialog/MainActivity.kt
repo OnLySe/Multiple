524ee1e1fbb4340d81192ca11abb21dfbbe32e57
fmzq <787031941@qq.com>
2020-09-30 18:47:56 +0800

    Dialog: 列表

--- Dialog/src/main/java/com/zzq/dialog/MainActivity.kt
+++ Dialog/src/main/java/com/zzq/dialog/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.zzq.dialog
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.zzq.dialog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.firstListener = View.OnClickListener {
+            FirstDialogFragment().show(supportFragmentManager,"first")
+        }
     }
 }
