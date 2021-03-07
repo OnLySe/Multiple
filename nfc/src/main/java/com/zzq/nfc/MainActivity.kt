90a5e9bc9ded169d70e4c5a9877f9b52f6399b86
JokeenZhang <zzq787an150@gmail.com>
2021-03-07 23:10:08 +0800

    reafactor: 更改build.gradle

--- nfc/src/main/java/com/zzq/nfc/MainActivity.kt
+++ nfc/src/main/java/com/zzq/nfc/MainActivity.kt
@@ -7,7 +7,9 @@ import android.nfc.NfcAdapter
 import android.nfc.Tag
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.zzq.common.utils.showLongToast
 
 //java.lang.SecurityException: NFC permission required: Neither user 10005 nor current process has android.permission.NFC.
 class MainActivity : AppCompatActivity() {
@@ -16,10 +18,21 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mWriteTagFilters: Array<IntentFilter>
     private lateinit var mNfcPendingIntent: PendingIntent
 
+    private lateinit var tvNfcInfo: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvNfcInfo = findViewById(R.id.tv_nfc_info)
 
+        val temp = NfcAdapter.getDefaultAdapter(this)
+        if (temp == null) {
+            showLongToast("机型不支持NFC！")
+            tvNfcInfo.text = "机型不支持NFC！"
+            return
+        } else {
+            mNfcAdapter = temp
+        }
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this)
         val tagDetected = IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED)
         mWriteTagFilters = arrayOf(tagDetected)
@@ -29,7 +42,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        mNfcAdapter.enableForegroundDispatch(this, mNfcPendingIntent, mWriteTagFilters, null)
+        if (this::mNfcAdapter.isInitialized) {
+            mNfcAdapter.enableForegroundDispatch(this, mNfcPendingIntent, mWriteTagFilters, null)
+        }
     }
 
     override fun onNewIntent(intent: Intent?) {
